bf41e299479e9a412a4fe771cfa226d7fa1ea0c7
xallve <prodanov1540@gmail.com>
2022-08-09 15:32:16 +0300

    --

--- News_api/News_api/settings.py
+++ News_api/News_api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from .secrets import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!a9uv9aqk7))khtx9+6+5xfhb_%52j94w(zr9g3qv2&$472*0k'
+# SECRET_KEY is hidden
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
